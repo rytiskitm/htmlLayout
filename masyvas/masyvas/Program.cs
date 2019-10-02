8eb5a168c9295468c714009d0c6b811149b55121
Moksleivis <moksleivis@KITM.local>
2019-10-02 15:07:15 +0300

    C#

---
+++ masyvas/masyvas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masyvas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1,1,2,3,6,8,99,6,6,6,3,3};
+            int[] unique;
+            int[] dontRepeat = numbers.Distinct().ToArray();
+
+            Console.WriteLine("Pradinis masyvas");
+            Console.WriteLine(string.Join(" ", numbers));
+            
+            Console.WriteLine("Nesikartojantys elementai");
+            Console.WriteLine(string.Join(" ", dontRepeat));
+            /*
+            Console.WriteLine("Unikalus elementai ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            */
+            Console.ReadLine();
+        }
+    }
+}
+/*
+ * susirast visus nepasikartojancius masyvo elementus
+ */
